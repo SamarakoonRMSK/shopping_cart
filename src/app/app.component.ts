01adae3febd0e3dc289b504e2f6dd3fe67184180
SamarakoonRMSK <rmsksamarakoon@gmail.com>
2025-07-21 16:11:44 +0530

    style: update app component styles and structure for improved layout and responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ShoppingCartComponent,ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
